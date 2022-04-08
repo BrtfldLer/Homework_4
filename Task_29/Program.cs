975b740c02bab92721d3480bdc6548c0896241bd
Valeriia <bartfeld.ler@yandex.ru>
2022-04-08 09:13:00 +0200

    Task_29 done

---
+++ Task_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+int [] array = new int[8];
+int len = array.Length;
+int index = 0;
+while (index < len)
+{
+    array[index] = new Random().Next(-1000,1001);
+    index++;
+}
+for (int i=0; i<len; i++)
+{
+Console.Write($" {array[i]} ");
+}
